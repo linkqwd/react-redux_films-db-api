e4621e27a345b170d9f56c893296663224c88ff3
linkqwd <linkqwd@gmail.com>
2019-07-23 23:59:06 +0300

    Updates

---
+++ precache-manifest.67af2cd473b58a28a1787e4fa6394178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5deb6fb2b8f0e731e951115196cfef65",
+    "url": "/react-redux_films-db-api/index.html"
+  },
+  {
+    "revision": "08d8fb527b11aeeece9f",
+    "url": "/react-redux_films-db-api/static/css/main.2e2213b9.chunk.css"
+  },
+  {
+    "revision": "5248a0f1bae92e804a7c",
+    "url": "/react-redux_films-db-api/static/js/2.d5d5d1a5.chunk.js"
+  },
+  {
+    "revision": "08d8fb527b11aeeece9f",
+    "url": "/react-redux_films-db-api/static/js/main.038ff439.chunk.js"
+  },
+  {
+    "revision": "df98314486fdaf4e1334",
+    "url": "/react-redux_films-db-api/static/js/runtime~main.7807116e.js"
+  }
+]);
